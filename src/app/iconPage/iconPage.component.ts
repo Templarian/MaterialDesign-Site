ff0430b61c491594f8056858e3f0e5a48e888bf3
Austin Andrews <admin@templarian.com>
2017-10-09 23:55:28 -0500

    Mock icon endpoint / wire up page.

--- src/app/iconPage/iconPage.component.ts
+++ src/app/iconPage/iconPage.component.ts
@@ -1,9 +1,28 @@
 import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+import { IconService } from "app/shared/icon.service";
 
 @Component({
   selector: 'mdi-icon-page',
-  templateUrl: './iconPage.component.html'
+  templateUrl: './iconPage.component.html',
+  providers: [
+    IconService
+  ]
 })
-export class IconPageComponent  {
-    
+export class IconPageComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private iconService: IconService) { }
+
+  iconName: string = '...';
+
+  ngOnInit() {
+    const packageId: string = this.route.snapshot.data['package'];
+    const iconName: string = this.route.snapshot.params['iconName'];
+    this.iconService.getIconByName(packageId, iconName).subscribe(icon => {
+      this.iconName = icon.name;
+    });
+  }
+
 }
