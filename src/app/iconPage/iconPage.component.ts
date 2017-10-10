4f725e27df522b7a6cc48b6b3d9b7104a9597334
Austin Andrews <admin@templarian.com>
2017-10-10 00:05:17 -0500

    Bind icon to preview box.

--- src/app/iconPage/iconPage.component.ts
+++ src/app/iconPage/iconPage.component.ts
@@ -16,12 +16,14 @@ export class IconPageComponent {
     private iconService: IconService) { }
 
   iconName: string = '...';
+  data: string = 'M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z';
 
   ngOnInit() {
     const packageId: string = this.route.snapshot.data['package'];
     const iconName: string = this.route.snapshot.params['iconName'];
     this.iconService.getIconByName(packageId, iconName).subscribe(icon => {
       this.iconName = icon.name;
+      this.data = icon.data;
     });
   }
 
