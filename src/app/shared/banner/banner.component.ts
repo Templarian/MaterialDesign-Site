96146aadaad27ba4268616d009eb833ecfceba79
Austin Andrews <admin@templarian.com>
2017-10-11 01:09:47 -0500

    Display icon in banner.

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -8,6 +8,7 @@ import { Component, Input } from '@angular/core';
 export class BannerComponent {
   
   @Input('title') name: string = 'Loading...';
+  @Input('icon') data: string = null;
   subtitle: string = null;
 
 }
