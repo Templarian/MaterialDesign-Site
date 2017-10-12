79a653b749bdd00c1289b74ae4193648c722df22
Austin Andrews <admin@templarian.com>
2017-10-12 11:47:42 -0500

    mdi tag accepts numbers.

--- src/app/viewerPage/viewerPage.component.ts
+++ src/app/viewerPage/viewerPage.component.ts
@@ -79,7 +79,7 @@ export class ViewerPageComponent {
               </${m2}>`;
     }
   },{
-    find: new RegExp('mdi:([a-z-]+)', 'g'),
+    find: new RegExp('mdi:([a-z0-9-]+)', 'g'),
     replace: (m, icon) => {
       this.icons.push(icon);
       return `<a href="icon/${icon}"><svg class="icon icon-spin" data-icon="${m}" viewBox="0 0 24 24"><path d="M12,4V2A10,10 0 0,0 2,12H4A8,8 0 0,1 12,4Z" /></svg></a>`;
