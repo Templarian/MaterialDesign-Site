a8961f8908a2da7f466ced7921d504ac356442f8
Austin Andrews <admin@templarian.com>
2017-11-21 20:32:18 -0600

    Hide icon count for 0.

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -5,6 +5,8 @@ export class User {
     public twitter: string = null;
     public name: string = null;
     public base64: string = null;
+    public iconCount: number = null;
+    public description: string = null;
 
     from(user: User): User {
         this.id = user.id;
@@ -12,6 +14,8 @@ export class User {
         this.twitter = user.twitter;
         this.name = user.name;
         this.base64 = user.base64;
+        this.iconCount = user.iconCount;
+        this.description = user.description;
         return this;
     }
 }
