6001528731e951bdafd39071ca101c96c9f126c9
Austin Andrews <admin@templarian.com>
2018-07-29 12:17:47 -0500

    Playing with animations.

--- src/app/homePage/homePage.component.ts
+++ src/app/homePage/homePage.component.ts
@@ -1,4 +1,28 @@
 import { Component, Input } from '@angular/core';
+import {
+  mdiAccount, mdiAccountOutline,
+  mdiAccountBox, mdiAccountBoxOutline,
+  mdiAccountMultiple, mdiAccountMultipleOutline,
+  mdiAccountMultiplePlus, mdiAccountMultiplePlusOutline,
+  mdiAccountPlus, mdiAccountPlusOutline,
+  mdiAccountSearch, mdiAccountSearchOutline,
+  mdiAlert, mdiAlertOutline,
+  mdiAlertCircle, mdiAlertCircleOutline,
+  mdiArrowDownBoldBox, mdiArrowDownBoldBoxOutline,
+  mdiArrowUpBoldBox, mdiArrowUpBoldBoxOutline,
+  mdiArrowLeftBoldBox, mdiArrowLeftBoldBoxOutline,
+  mdiArrowRightBoldBox, mdiArrowRightBoldBoxOutline,
+  mdiArrowDownBold, mdiArrowDownBoldOutline,
+  mdiArrowUpBold, mdiArrowUpBoldOutline,
+  mdiArrowLeftBold, mdiArrowLeftBoldOutline,
+  mdiArrowRightBold, mdiArrowRightBoldOutline,
+  mdiBookmark, mdiBookmarkOutline,
+  mdiBookmarkOff, mdiBookmarkOffOutline,
+  mdiBriefcase, mdiBriefcaseOutline,
+  mdiCards, mdiCardsOutline,
+  mdiCart, mdiCartOutline,
+  mdiCheckCircle, mdiCheckCircleOutline,
+} from '@mdi/js'
 
 @Component({
   selector: 'mdi-home-page',
@@ -7,4 +31,33 @@ import { Component, Input } from '@angular/core';
 })
 export class HomePageComponent {
   title: string = 'Home';
+  icons: regularOutline[] = [
+    { regular: mdiAccount, outline: mdiAccountOutline },
+    { regular: mdiAccountBox, outline: mdiAccountBoxOutline },
+    { regular: mdiAccountMultiple, outline: mdiAccountMultipleOutline },
+    { regular: mdiAccountMultiplePlus, outline: mdiAccountMultiplePlusOutline },
+    { regular: mdiAccountPlus, outline: mdiAccountPlusOutline },
+    { regular: mdiAccountSearch, outline: mdiAccountSearchOutline },
+    { regular: mdiAlertCircle, outline: mdiAlertCircleOutline },
+    { regular: mdiAlert, outline: mdiAlertOutline },
+    { regular: mdiArrowDownBoldBox, outline: mdiArrowDownBoldBoxOutline },
+    { regular: mdiArrowUpBoldBox, outline: mdiArrowUpBoldBoxOutline },
+    { regular: mdiArrowLeftBoldBox, outline: mdiArrowLeftBoldBoxOutline },
+    { regular: mdiArrowRightBoldBox, outline: mdiArrowRightBoldBoxOutline },
+    { regular: mdiArrowDownBold, outline: mdiArrowDownBoldOutline },
+    { regular: mdiArrowUpBold, outline: mdiArrowUpBoldOutline },
+    { regular: mdiArrowLeftBold, outline: mdiArrowLeftBoldOutline },
+    { regular: mdiArrowRightBold, outline: mdiArrowRightBoldOutline },
+    { regular: mdiBookmark, outline: mdiBookmarkOutline },
+    { regular: mdiBookmarkOff, outline: mdiBookmarkOffOutline },
+    { regular: mdiBriefcase, outline: mdiBriefcaseOutline },
+    { regular: mdiCards, outline: mdiCardsOutline },
+    { regular: mdiCart, outline: mdiCartOutline },
+    { regular: mdiCheckCircle, outline: mdiCheckCircleOutline }
+  ]
+}
+
+interface regularOutline {
+  regular: string;
+  outline: string;
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -9,11 +9,14 @@ import { Router, NavigationEnd } from '@angular/router';
 export class HeaderComponent  {
   public name = 'Material Design Icons';
   public isNav = true;
+  public isHome = true;
 
   constructor(private router: Router) {
     router.events.subscribe((val) => {
-        //console.log(val instanceof NavigationEnd);
-        this.isNav = true;
+      this.isNav = true;
+      if (val instanceof NavigationEnd) {
+        this.isHome = val.url === '/';
+      }
     });
   }
 }
