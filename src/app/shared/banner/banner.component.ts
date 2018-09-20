83fcc7d6e4985e5abaf453ae7070185457cc4964
Austin Andrews <admin@templarian.com>
2018-09-19 19:27:44 -0500

    Don't load ad on localhost.

--- src/app/shared/banner/banner.component.ts
+++ src/app/shared/banner/banner.component.ts
@@ -13,4 +13,15 @@ export class BannerComponent {
   subtitle: string = null;
 
   isLocal: boolean = window.location.href.match(/localhost/) !== null
+
+  ngAfterViewInit() {
+    if (!this.isLocal) {
+      var s = document.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.id = '_carbonads_js';
+      s.src = '//cdn.carbonads.com/carbon.js?serve=CKYIPKJW&placement=materialdesigniconscom';
+      document.getElementById('carbon').appendChild(s);
+    }
+  }
 }
