c114c530edb4d2e1a6678e20ad562919615fec27
Austin Andrews <admin@templarian.com>
2019-07-10 00:54:46 -0500

    Fix icon service.

--- src/app/shared/icon.service.ts
+++ src/app/shared/icon.service.ts
@@ -77,7 +77,14 @@ export class IconService {
 
   async addIcon(icon: Icon): Promise<Icon> {
     let res = await this.http.post<Icon>('/api/admin/icon', {
-      icon
+      icon: {
+        id: icon.id,
+        packageId: icon.packageId,
+        name: icon.id,
+        description: icon.description,
+        data: icon.data,
+        published: icon.published
+      }
     }).toPromise();
     return new Icon().from(res);
   }
