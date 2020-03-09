940d0ce1b059ae1a8c2786a7fcb5f201e77958d4
Austin Andrews <admin@templarian.com>
2020-03-09 01:50:16 -0500

    v5.0.45

--- src/app/viewerPage/viewerPage.service.ts
+++ src/app/viewerPage/viewerPage.service.ts
@@ -27,7 +27,7 @@ export class ViewerService {
 
   private extractData(text: string) {
     // TODO: Replace with db config
-    text = text.replace(/\{\{version\}\}/g, '4.9.95');
+    text = text.replace(/\{\{version\}\}/g, '5.0.45');
     return text;
   }
 
