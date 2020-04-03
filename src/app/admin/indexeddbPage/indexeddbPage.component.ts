33bda9a43d137ec810c0f390c1cbc165adf34bd6
Austin Andrews <admin@templarian.com>
2020-04-03 00:25:05 -0500

    Move files around.

--- src/app/admin/indexeddbPage/indexeddbPage.component.ts
+++ src/app/admin/indexeddbPage/indexeddbPage.component.ts
@@ -6,7 +6,6 @@ import { Alias } from 'app/shared/models/alias.model';
 import { Tag } from 'app/shared/models/tag.model';
 import { DatabaseService } from 'app/shared/database.service';
 import { Router } from '@angular/router';
-import { Database } from './database';
 import { Font } from 'app/shared/models/font.model';
 
 @Component({
@@ -50,114 +49,17 @@ export class AdminIndexeddbPageComponent {
     await this.loginService.logout();
   }
 
-  db = new Database();
-
-  async asyncThrottledMap<T, U>(maxCount: number, array: T[], f: (x: T) => Promise<U>) {
-    let inFlight = new Set<Promise<U>>();
-    const result: Promise<U>[] = [];
-
-    // Sequentially add a Promise for each operation.
-    for (let elem of array) {
-
-        // Wait for any one of the promises to complete if there are too many running.
-        if (inFlight.size >= maxCount) {
-            await Promise.race(inFlight);
-        }
-
-        // This is the Promise that the user originally passed us back.
-        const origPromise = f(elem);
-        // This is a Promise that adds/removes from the set of in-flight promises.
-        const handledPromise = wrap(origPromise);
-        result.push(handledPromise);
-    }
-
-    return Promise.all(result);
-
-    async function wrap(p: Promise<U>) {
-        inFlight.add(p);
-        const result = await p;
-        inFlight.delete(p);
-        return result;
-    }
-  }
-
   async sync() {
-    console.log('start');
-    const font = new Font().from({
-      id: 'D051337E-BC7E-11E5-A4E9-842B2B6CFE1B'
-    } as Font);
-    const hashes = await this.databaseService.getHashes(font);
-    const hashIds = Object.keys(hashes);
-    const localHashObj = await this.db.hashes.toArray();
-    const localHashes = {};
-    localHashObj.forEach((item) => {
-      localHashes[item.id] = item.hash;
-    });
-    const localHashIds = Object.keys(localHashes);
-    const updateIds = [];
-    hashIds.forEach((id) => {
-      if (!(id in localHashes) || localHashes[id] !== hashes[id]) {
-        updateIds.push(id);
-      }
-    });
-    const removeIds = [];
-    localHashIds.forEach((id) => {
-      if (!(id in hashes)) {
-        removeIds.push(id);
-      }
-    });
-    await this.db.icons.bulkDelete(removeIds);
-    if (updateIds.length < 500) {
-      // Do a partial update patch of data
-      let i, j, chunkIds = [], chunk = 100;
-      for (i = 0, j = updateIds.length; i < j; i += chunk) {
-        chunkIds.push(updateIds.slice(i, i + chunk));
-      }
-      const chunkPromises = chunkIds.map((ids) => {
-        return this.databaseService.getIcons(font, ids);
-      });
-      await Promise.all(chunkPromises);
-    } else {
-      // Sync every icon into the database
-      let size = 500,
-        pages = Math.ceil(hashIds.length / size),
-        chunkPromises = [];
-      for (let page = 1; page < pages; page++) {
-        chunkPromises.push(
-          this.databaseService.getIconsByPage(font, page, size)
-        );
-      }
-      await this.asyncThrottledMap<Promise<Icon[]>, void>(2, chunkPromises, (p) =>
-        p.then(icons => {
-          this.db.icons.bulkAdd(
-            icons.map(icon => ({
-              id: icon.id.substr(0, 8),
-              idFull: icon.id,
-              name: icon.name,
-              data: icon.data,
-              codepoint: icon.codepoint,
-              aliases: JSON.stringify(icon.aliases),
-              tags: JSON.stringify(icon.tags)
-            })
-          ));
-        })
-      );
-    }
-    await this.db.hashes.bulkPut(hashIds.map(id => ({ id, hash: hashes[id] })));
-    await this.db.hashes.bulkDelete(removeIds);
-    console.log('done');
+    await this.databaseService.sync();
   }
 
   async where() {
-    this.db.icons.where('name').equals('account').first().then((item) => {
-      console.log(item);
-    }, (err) => {
-      console.log('not found')
-    });
+    const icon = await this.databaseService.getIconByName('account');
+    console.log(icon);
   }
 
   async delete() {
-    await this.db.delete();
+    await this.databaseService.delete();
   }
 
 }
