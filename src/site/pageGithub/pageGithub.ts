5d85a0615477b283592dc9a5866032633ffe5ad6
Austin Andrews <admin@templarian.com>
2020-07-18 23:26:35 -0500

    typos

--- src/site/pageGithub/pageGithub.ts
+++ src/site/pageGithub/pageGithub.ts
@@ -69,6 +69,9 @@ export default class SitePageGithub extends HTMLElement {
 
   connectedCallback() {
     this.$markdown.file = GITHUB_FILE;
+    this.$edit.href = `https://github.com/Templarian/MaterialDesign-Site/tree/master/src/${GITHUB_FILE}`;
+    const h1 = 'GitHub Icon Preview Generator Instructions';
+    this.$suggest.href = `https://github.com/Templarian/MaterialDesign-Site/issues/new?title=Suggested%20Change%20to%20%22${h1}%22&body=%3C%21--%20Describe%20how%20you%20would%20improve%20the%20documentation%20here%20--%3E`;
     this.$wipYes.addEventListener('click', () => {
       this.wip = true;
     });
