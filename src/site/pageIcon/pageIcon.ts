f0c8a08dbd387b424738e9fae2b9d8fea3071267
Austin Andrews <admin@templarian.com>
2020-07-19 22:43:40 -0500

    minor styles

--- src/site/pageIcon/pageIcon.ts
+++ src/site/pageIcon/pageIcon.ts
@@ -84,15 +84,22 @@ export default class SitePageIcon extends HTMLElement {
       icon.tags.forEach((tag) => {
         const li = document.createElement('li');
         const a = document.createElement('a');
-        a.innerText = tag.name || 'error';
+        const span = document.createElement('span');
+        const mdiIcon = document.createElement('mdi-icon') as MdiIcon;
+        span.innerText = tag.name || 'error';
         a.href = `/icons?tag=${tag.url}`;
+        mdiIcon.path = 'M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z';
+        a.appendChild(span);
+        a.appendChild(mdiIcon)
         li.appendChild(a);
         this.$tagList.appendChild(li);
       });
       // Aliases
       icon.aliases.forEach((alias) => {
         const li = document.createElement('li');
-        li.innerText = alias.name || 'error';
+        const span = document.createElement('span');
+        span.innerText = alias.name || 'error';
+        li.appendChild(span);
         this.$aliasList.appendChild(li);
       });
     }
