c519afe7f6b24a0677e9a124b270e426c5798683
Austin Andrews <admin@templarian.com>
2020-10-14 23:56:08 -0500

    Progress.

--- src/site/pageHistory/pageHistory.ts
+++ src/site/pageHistory/pageHistory.ts
@@ -1,32 +1,61 @@
 import { Component, Prop, Part } from '@mdi/element';
- 
+
 import template from "./pageHistory.html";
 import style from './pageHistory.css';
 
 import { Icon } from '@mdi/components/mdi/shared/models/icon';
+import { Modification } from '@mdi/components/mdi/shared/models/modification';
+import { ModificationType } from '@mdi/components/mdi/shared/enums/modificationType';
 import { http } from '@mdi/components/mdi/shared/http';
+import '@mdi/components/mdi/card';
+import '@mdi/components/mdi/button';
+import '@mdi/components/mdi/inputCheck';
+import MdiButton from '@mdi/components/mdi/button';
+import MdiInputCheck from '@mdi/components/mdi/inputCheck';
 
 @Component({
   selector: 'site-page-history',
   style,
-  template 
+  template
 })
 export default class SitePageHistory extends HTMLElement {
   @Prop() icons: Icon[] = [];
   @Prop() navigationItems: any[] = [];
-  
+
   @Part() $header: HTMLHeadingElement;
   @Part() $loading: HTMLDivElement;
   @Part() $error: HTMLDivElement;
   @Part() $icon: HTMLDivElement;
-  
+  @Part() $more: MdiButton;
+
+  @Part() $news: MdiInputCheck;
+  @Part() $iconCreated: MdiInputCheck;
+  @Part() $iconModified: MdiInputCheck;
+  @Part() $iconRenamed: MdiInputCheck;
+  @Part() $iconDeleted: MdiInputCheck;
+  @Part() $iconAliasCreated: MdiInputCheck;
+
+  types: Array<[string, boolean, ModificationType[]]> = [
+    ['news', true, [ModificationType.News]],
+    ['iconCreated', true, [ModificationType.IconCreated]],
+    ['iconModified', true, [ModificationType.IconModified]],
+    ['iconRenamed', true, [ModificationType.IconRenamed]],
+    ['iconDeleted', true, [ModificationType.IconDeleted]],
+    ['iconAliasCreated', false, [ModificationType.IconAliasCreated, ModificationType.IconAliasDeleted]],
+    ['iconTagCreated', false, [ModificationType.IconTagCreated, ModificationType.IconTagDeleted]],
+  ];
+
   connectedCallback() {
+    this.types.forEach((type) => {
+      this[`$${type[0]}`].addEventListener('change', this.handleToggle.bind(this));
+      this[`$${type[0]}`].value = type[1];
+    });
     this.load();
   }
 
   async load() {
-    const packageId = '38EF63D0-4744-11E4-B3CF-842B2B6CFE1B';
-    const modifications = await http.get<any>(`/api/package/38EF63D0-4744-11E4-B3CF-842B2B6CFE1B/modification?modificationId=B4DEB3A8-A146-4086-9D7B-B67842A9CCB8,66B9FA99-1FAA-4D8F-B87F-B6F3CA444624,AFFE875E-01BC-4A34-9BE3-27625A155B28,1506F66B-CC2A-4575-A20A-DC138628977A,F7B6D49B-A86F-49AC-AF92-6B9D0DF6188B,B1CE1713-A18A-4E9D-9E26-D0B4E44A1FAC&page=1&size=100`)
+    const modifications = await this.requestPage(1);
+    console.log(modifications);
     /*const icon = await http.get<Icon>(`/api/package/${packageId}/name/${name}`);
     const { error } = icon as any;
     this.$loading.style.display = 'none';
@@ -36,5 +65,39 @@ export default class SitePageHistory extends HTMLElement {
       const related = await http.get<Icon[]>(`/api/icon/${icon.id}/base`);
       this.$icon.style.display = 'block';
     }*/
+  }
+
+  handleToggle(e: MouseEvent) {
+    const target = e.target as MdiInputCheck;
+    const part = target.getAttribute('part');
+    const type = this.types.find(t => t[0] === part);
+    if (type) {
+      type[1] = target.value as boolean;
+    }
+    this.load();
+  }
+
+  async requestPage(page: number) {
+    const size = 100;
+    const types = this.types.reduce(
+      function (arr: string[], t) {
+        if (t[1]) {
+          arr.push(...t[2]);
+        }
+        return arr;
+      },
+      []
+    );
+    const modifications = (await http.get<Modification[]>(
+      '/api/package/38EF63D0-4744-11E4-B3CF-842B2B6CFE1B/modification',
+      {
+        params: {
+          modificationId: types,
+          page: `${page}`,
+          size: `${size}`
+        }
+      })
+    ).map(m => new modifications().from(m));
+    return modifications;
   }
 }
